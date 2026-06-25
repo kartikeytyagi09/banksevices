c34c19098148bebffd10734c20450b521d5459fc
kartikey tyagi <tkartikey92@gmail.com>
2026-06-25 19:32:37 +0530

    transaction model is created

---
+++ src/models/transaction.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    fromAccount:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:account,
+        required:[true, "fromAccount is required"]
+    },
+    toAccount:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:account,
+        required:[true, "toAccount is required"]
+    },
+    amount:{
+        type:Number,
+        required:[true, "amount is required"],
+        min:[0, "amount must be greater than 0"]
+    },
+    status:{
+        type:String,
+        enum:["SUCCESS", "FAILED", "PENDING"],
+        default:"PENDING"
+    },
+    idempotencyKey:{
+        type:String,
+        required:[true,"idempotencyKey is required"],
+        index:true,
+        unique:true
+    }
+},{
+        timestamps:true
+    }
+);
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
