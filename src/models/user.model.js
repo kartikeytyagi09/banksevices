3e35663446b807905f2452179957e001216fae31
kartikey tyagi <tkartikey92@gmail.com>
2026-06-28 21:00:35 +0530

    role in user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true, "Password is required"],
         minlength:[6, "Password must be at least 6 characters long"]
+    },
+    role:{
+        type:String,
+        enum:["USER", "ADMIN"],
+        default:"USER" 
     }
 },
     {
